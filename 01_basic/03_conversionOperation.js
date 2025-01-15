1a690791febc1a4fda19cf80b60013d9a15914ea
sachinkr101 <81379002+sachinkr101@users.noreply.github.com>
2025-01-15 18:40:26 +0000

    Conversion

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score="A33a"
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// conversion 
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+//  true => 1; false => 0
+
+let isLoggedIn = "Sachin"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+  console.log(booleanIsLoggedIn);
+
+// Boolean Conversion
+// 1 => true; 0 => false
+// "" => false
+// "Sachin" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
