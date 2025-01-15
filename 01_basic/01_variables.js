c213b55cb02989d04f2038889cecc46b82d3106c
sachinkr101 <81379002+sachinkr101@users.noreply.github.com>
2025-01-15 16:08:11 +0000

    variables for JS

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 1234
+let Naam="sachin"
+var email="sachin@gmail.com"
+address = "Hyderabad"
+Naam="rahul"
+email="Test@test.com"
+address="Patna"
+//accountId= 2 not allowed
+
+// console.log(Naam)
+console.table([Naam, email, address, accountId]);
+
+/*
+Prefer to use let pinstead of var because of the scope of the variable 
+*/
