81a9c7b0a80104209ab546576d608d4d06719deb
sachinkr101 <81379002+sachinkr101@users.noreply.github.com>
2025-01-16 20:42:02 +0000

    string

---
+++ 01_basic/05_String.js
@@ -0,0 +1,36 @@
+let name="Sachin"
+let score = "58"
+
+// console.log(name + " "+ score+ "Ok");  complicated and confusing method 
+
+console.log(`My name is ${name} my score is ${score}`)
+
+
+const gameName = new String('hitesh-hc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
